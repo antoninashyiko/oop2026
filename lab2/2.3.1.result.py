65b91848fa8d08ee772067873ce7d139e9eadfdb
tonasijko74-crypto <tonasijko74@gmail.com>
2026-03-24 16:57:04 +0200

    edited lab2

--- lab2/2.3.1.result.py
+++ lab2/2.3.1.result.py
@@ -25,57 +25,66 @@ class center:
         self.y = y
         self.r1 = r1
         self.r3 = r3
+        self.color="yellow"
+        self.p1=self.x-7
+        self.p2=self.y-self.r1
     def draw(self):
         t.speed(3)
-        t.pencolor("yellow")
-        t.fillcolor("yellow")
+        t.pencolor(self.color)
+        t.fillcolor(self.color)
         t.up()
-        t.setpos(self.x - 7, self.y - self.r1)
+        t.setpos(self.p1, self.p2)
         t.down()
         t.begin_fill()
         t.circle(self.r3)
         t.end_fill()
         t.up()
 class stem:
-    def __init__(self, x, y, r1):
+    def __init__(self, x, y, r1, base_x, base_y):
         self.x = x
         self.y = y
         self.r1 = r1
-    def draw(self, base_x, base_y):
+        self.pen_color="green"
+        self.pos=self.y-self.r1
+        self.base_x=base_x
+        self.base_y=base_y
+    def draw(self):
         t.speed(3)
-        t.pencolor("green")
+        t.pencolor(self.pen_color)
         t.pensize(2)
         t.up()
-        t.setpos(self.x, self.y - self.r1)
+        t.setpos(self.x, self.pos)
         t.down()
-        t.goto(base_x, base_y)
+        t.goto(self.base_x, self.base_y)
         position = t.pos()
         t.up()
         return position
 class leaf:
-    def __init__(self, x, y, r1, r2):
+    def __init__(self, x, y, r1, r2, position):
         self.x = x
         self.y = y
         self.r1 = r1
         self.r2 = r2
-    def draw(self, position):
+        self.color="green"
+        self.x1, self.y1 = position[0], position[1]
+        self.x2, self.y2 = self.x, self.y - self.r1
+        self.cx = self.x1 + 0.5 * (self.x2 - self.x1)
+        self.cy = self.y1 + 0.5 * (self.y2 - self.y1)
+        self.angle = math.degrees(math.atan2(self.y2 - self.y1, self.x2 - self.x1))
+    def draw(self):
         t.speed(3)
-        t.pencolor("black")
-        t.fillcolor("green")
-        x1, y1 = position[0], position[1]
-        x2, y2 = self.x, self.y - self.r1
-        cx = x1 + 0.5 * (x2 - x1)
-        cy = y1 + 0.5 * (y2 - y1)
-        angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
-        t.goto(cx, cy)
-        t.setheading(angle+180)
-        t.goto(cx, cy)
+        t.pencolor(self.color)
+        t.fillcolor(self.color)
+        t.goto(self.cx, self.cy)
+        t.setheading(self.angle+180)
+        t.up()
+        t.goto(self.cx, self.cy)
         t.down()
         t.begin_fill()
         t.circle(self.r2)
         t.end_fill()
         t.up()
-class Flower:
+class Flower():
     def __init__(self, x, y, r1, r2, r3, color):
         self.x = x  # x - координата x центру кола
         self.y = y  # y - координата y центру кола
@@ -83,13 +92,13 @@ class Flower:
         self.r2 = r2 # радіус листка
         self.r3 = r3 # радіус серединки
         self.color=color # колір пелюстки
-        self.stem=stem(x, y, r1)
-        self.leaf=leaf(x, y, r1, r2)
+        self.stem=stem(x, y, r1, 0, -200)
+        pos = self.stem.draw()
+        self.leaf=leaf(x, y, r1, r2, pos)
         self.center=center(x, y, r1, r3)
         self.petal=petal(x, y, r1, color)
     def draw(self):
-        pos = self.stem.draw(0, -200)
-        self.leaf.draw(pos)
+        self.leaf.draw()
         self.petal.draw()
         self.center.draw()
 
